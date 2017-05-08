0a9b9e001de779cf777e10de1ba065c6693e7638
a1asd <iamhldisch@hotmail.de>
2017-05-08 20:48:15 +0200

    why do i need a sample.fxml anyway...

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent fxmlroot = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent fxmlroot = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
         primaryStage.setTitle("Hello World");
 
